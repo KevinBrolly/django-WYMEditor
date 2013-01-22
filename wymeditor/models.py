b405e596c5f8f3d83ce5f75b9608d10f4f59382b
Kevin Brolly <kevin.brolly@gmail.com>
2013-01-22 15:53:57 +0000

    Update wymeditor/models.py
    Added introspection rules to ensure custom field works with south.

--- wymeditor/models.py
+++ wymeditor/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.contrib.admin import widgets as admin_widgets
 
 from widgets import WYMEditorWidget, AdminWYMEditor
+try:
+    from south.modelsinspector import add_introspection_rules
+    add_introspection_rules([], ['^wymeditor\.models\.WYMEditorField'])
+except ImportError:
+    pass
 
 
 class WYMEditorField(models.TextField):
